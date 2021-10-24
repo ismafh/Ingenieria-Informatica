a716d79e1d63877c0fd6fd2d14fb1824066d5e8c
ElTitoIsma <ismaelfh.2001@gmail.com>
2021-10-24 22:28:28 +0200

    prueba

--- Segundo/LTPP/Practicas/Practica 1 python/pruebas.py
+++ Segundo/LTPP/Practicas/Practica 1 python/pruebas.py
@@ -1,99 +1,38 @@
-import random
-#Introducción y creacion de lista
-print('Hola campeón. Vamos a jugar.\nDéjame que piense... humm... ya he elegido la lista de números\nAdivínala:')
-lst=[]
-i=0
-while i<5:
-    lst=lst+[random.randint(1,10)]
-    i=i+1
-print(lst)
-#Creación de la lista introducida por el usuario
-for j in range(0, 5):
-        numeros=str(input())
-        z=0
-        n=0
-        aux=' '
-        listnumeros=[0,0,0,0,0]
-        
-        while z<len(numeros):    #Asignacion de cada posicion de la lista
-            for i in numeros:
-                if i ==' ':
-                    listnumeros.append(aux)
-                    aux = ''
-                else:
-                    aux += i
-            if aux:
-                listnumeros.append(aux)
-            print(listnumeros)
-        else:
-            aux2=[0,0,0,0,0]
-            while v<int(len(listnumeros)):
-                aux[v]=int(listnumeros[v])
-                v=v+1
-        print(aux)
-        #Comparacion y final del juego
-        j=0
-        aux3=[0,0,0,0,0]
-        while j<5:
-            if lst[j]==listnumeros[j]:
-                aux3[j]=1
-                j=j+1
-            else:
-                aux3[j]=0
-                j=j+1
-        print (aux3)
-        if aux==[1,1,1,1,1]:
-            print('Has ganado')
-            break
-        else: 
-            print('Sigue intentandolo')
-            print(lst)
-        
-        
+import numpy as np
+import matplotlib.pyplot as plt
+Theta1=[]
+Theta2=[]
+def Intensity1(theta1):
+    a = 9e-05                     #slit width
+    lamda = float(670)* 1e-09     #wavelength
+    L = float(480)* 1e-03
+    alpha = ((np.pi * a) / lamda) * np.sin(theta1)
+    result= (np.sin(alpha) / alpha  )**2
+    U1=1e03*np.tan(Theta1)*np.array(L)
+
+def Intensity2(theta2):
+    a = 9e-05                     #slit width
+    lamda = float(670)* 1e-09     #wavelength
+    L = float(480)* 1e-03
+    d = 4e-04
+    alpha = ((np.pi * a) / lamda) * np.sin(theta2)
+    beta = ((np.pi * d) / lamda) * np.sin(theta2)
+    result= 4 * (np.sin(alpha) / alpha)**2 * (np.cos(beta))**2
+    U2=1e03*np.tan(Theta2)*np.array(L)
 
 
-            
-            
-                
-            
-        
-        
-        
+x_max = 8
+y_max = 1
 
+x3 = np.linspace(-x_max, x_max, 3000)
+y3 = np.linspace(-y_max, y_max, 3000)
+X, Y = np.meshgrid(x3, y3)
 
+fig = plt.figure(figsize=(10, 6))
 
+z = Intensity2(X)
+s = Intensity1(X)
 
-        
+plt.imshow(z, extent = [-x_max, x_max, -y_max, y_max], cmap = 'gray', interpolation = 'bilinear', aspect = 'auto')
 
-        
-        
-    
-   
-    
-
-
-
-    
-        
-        
-        
-
-    
-
-        
-    
-        
-    
-    
-        
-    
-    
-
-
-
-
-
-
-
-
-
+plt.show()
