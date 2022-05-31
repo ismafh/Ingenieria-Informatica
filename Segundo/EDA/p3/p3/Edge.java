46b1900b4b0e8d6a89733d93b80cd87b33770a62
ElTitoIsma <ismaelfh.2001@gmail.com>
2022-05-31 22:56:33 +0200

    xd

---
+++ Segundo/EDA/p3/p3/Edge.java
@@ -0,0 +1,15 @@
+package p3;
+public class Edge implements Comparable<Edge>{
+    
+    public Vertex getU() {
+
+    }
+
+    public Vertex getV() {
+
+    }
+
+    public int getW() {
+        
+    }
+}
